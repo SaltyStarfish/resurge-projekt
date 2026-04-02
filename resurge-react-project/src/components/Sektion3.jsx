a816ae38938827d0c3471c11a9d2d31199ece1de
lunexea <eaa25ids@students.eaaa.dk>
2026-04-02 10:57:57 +0200

    Create Sektion3.jsx
    startup

---
+++ resurge-react-project/src/components/Sektion3.jsx
@@ -0,0 +1,27 @@
+import {link} from "react-router-dom";
+
+export default function Sektion3() {
+
+    return (
+        <div class="3-Partskasse">
+            <sektion class="Boks1"> /*  */
+                <div id="Placeholderimage"></div>"
+                <h2>Skræddersyede løsninger</h2>
+                <p>Alt for mange bureauer arbejder med tunge løsninger, som ikke er tilpasset den enkelte virksomhed. Som indsamler data, der aldrig bliver brugt, men kun fylder og bruger strøm. Eller udliciterer webproduktionen til andre lande, hvor du mister kontrollen og overblikket. Den tendens vil vi gøre op med. 
+                    Hos Resurge mener vi, at tingene skal gøres ordentligt - og til en overkommelig pris. Så du har kontrollen hele vejen igennem og får en webløsning, der er tilpasset dine behov - ikke bare en standardpakke. Websitet skal passe til din virksomhed her og nu og kan udvides, når din virksomhed vokser. 
+                </p>
+            </sektion>
+            <section class="Boks2">
+                <h2>Performanceoptimering</h2>
+                <p>Når det overflødige skæres væk, bliver dit website automatisk lettere og performer bedre. Dertil kommer billed-, video- og typografi-optimering, som igen er med til at gøre dit website hurtigere - uden at gå på kompromis med kvaliteten. Vi holder os opdateret på den nyeste teknologi og de bedste værktøjer til at optimere både performance og energiforbrug.</p>
+                <div id="Placeholderimage"></div>
+            </section>
+            
+            <sektion class="Boks1"> /*  */
+                <div id="Placeholderimage"></div>"
+                <h2>Reduceret CO2-aftryk</h2>
+                <p>Klimabevidsthed fylder i arbejdstilgangen hos Resurge, og performance- og CO2-optimering går heldigvis hånd i hånd. Et website vil per definition aldrig blive bæredygtigt, og man siger, at det eneste bæredygtige website er et website, som ikke eksisterer. Med det i mente tror vi, at det mindst bæredygtige website er et website, som ikke opfylder dets formål. Derfor er det vores topprioritet at skabe effektive webløsninger, som skaber værdi for virksomheder og deres kunder. Samtidig arbejder vi aktivt med at gøre websites mindre datatunge for at opnå en bedre brugeroplevelse, højere performance og et mindre CO2-aftryk.</p>
+            </sektion>
+        </div>
+ )
+}
