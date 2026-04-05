cba8a7c48fb79b1ad9f0510dcc9542196b0e07b3
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-05 23:24:51 +0200

    Tekst + placeholder-billeder tilføjet

--- resurge-react-project/src/views/BlogView.jsx
+++ resurge-react-project/src/views/BlogView.jsx
@@ -1,16 +1,52 @@
-// Husk at importere alle Links og Components, der referes til
-import ComponentTemplate from "../components/ComponentTemplate"
-
+import FiftyFifty from "../components/FiftyFifty"
+import placeHolder from "../assets/images/placeholder.png"
 
 function BlogView() {
 
   return (
-    <>
-      {/* Opret sidens indhold herunder som "normal HTML" eller referer til components*/}
-      <h1>Blog</h1>
-      <ComponentTemplate/>
-    </>
-  )
-}
+    <div className="blog">
+        <section className="blog-intro">
+          <h1>Inspiration og guides</h1>
+          <p>Her har vi samlet vores praktiske tips til et hurtigere og mere energieffektivt website</p>
+        </section>
+
+        {/* Blogindlæg 1 */}
+        <FiftyFifty>
+          <div>
+            <img src={placeHolder} alt="Blogindlæg 1" className="w-[500px] h-[500px] object-cover" />
+          </div>
+          <div>
+            <h3>5 tips til at forbedre dit websites performance</h3>
+            <p>Optimer loadtid, konvertering og brugeroplevelsen med simple greb. Selv små ændringer kan gøre en stor forskel, både for dine brugere og dit klimaaftryk.</p>
+            <a href="blog/fem-tips" className="laes-mere">Læs mere →</a>
+          </div>
+        </FiftyFifty>
+
+        {/* Blogindlæg 2 */}
+        <FiftyFifty reverse>
+          <div className="w-full md:w-1/2 flex flex-col justify-center">
+            <h3>Få mere ud af dit website uden at sprænge budgettet</h3>
+            <p>Du behøver ikke nødvendigvis et stort budget for at forbedre dit website. Vi guider her til, hvor du får mest ud af din indsats.</p>
+            <a href="#" className="laes-mere">Læs mere →</a>
+          </div>
+          <div>
+            <img src={placeHolder} alt="Blogindlæg 1" className="w-[500px] h-[500px] object-cover" />
+          </div>
+        </FiftyFifty>
+
+        {/* Blogindlæg 3 */}
+        <FiftyFifty>
+          <div>
+            <img src={placeHolder} alt="Blogindlæg 1" className="w-[500px] h-[500px] object-cover" />
+          </div>
+          <div>
+            <h3>Få mere ud af dit website uden at sprænge budgettet</h3>
+            <p>Jo hurtigere dit website er, desto mindre energi udleder det. Se hvordan performance-optimering samtidig giver dig et mindre klimatungt site.</p>
+            <a href="#" className="laes-mere">Læs mere →</a>
+          </div>
+        </FiftyFifty>
+      </div>
+    );
+  }
 
 export default BlogView
