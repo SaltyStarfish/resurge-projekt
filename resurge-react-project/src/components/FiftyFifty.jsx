162a7befafeada932e85b08ec214e5ed8e413561
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-05 23:24:27 +0200

    Gap tilføjet

--- resurge-react-project/src/components/FiftyFifty.jsx
+++ resurge-react-project/src/components/FiftyFifty.jsx
@@ -1,7 +1,7 @@
 export default function FiftyFifty({ children, reverse }) {
   return (
     <div
-      className={`flex flex-col md:flex-row ${reverse ? "md:flex-row-reverse" : ""}`}
+      className={`flex flex-col md:flex-row ${reverse ? "md:flex-row-reverse" : ""} gap-6 md:gap-12 items-center`}
     >
       {children}
     </div>
