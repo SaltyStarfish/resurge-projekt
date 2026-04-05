75e5beaffd8813a97d8c6dc7403036d84753347c
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-05 21:37:39 +0200

    funktion ændret

--- resurge-react-project/src/components/FiftyFifty.jsx
+++ resurge-react-project/src/components/FiftyFifty.jsx
@@ -1,8 +1,7 @@
-export default function FiftyFifty({ children, reverse = false }) {
+export default function FiftyFifty({ children, reverse }) {
   return (
     <div
-      className={`flex flex-col md:flex-row ${reverse ? "md:flex-row-reverse" : ""
-      }`}
+      className={`flex flex-col md:flex-row ${reverse ? "md:flex-row-reverse" : ""}`}
     >
       {children}
     </div>
