b2065c301bc8a262b41c0d860fedd935edfa05b8
lunexea <eaa25ids@students.eaaa.dk>
2026-04-06 23:45:30 +0200

    Update ForsideView.jsx

--- resurge-react-project/src/views/ForsideView.jsx
+++ resurge-react-project/src/views/ForsideView.jsx
@@ -1,17 +1,14 @@
-import Værdier from "../components/Værdier"
-import infographic2 from "../assets/images/inforgraphic 2 - 2 stack.png"
+
 
 
 function ForsideView() {
-  const værdierBlocks = [
-    { title: "vores værdier", imageSrc: infographic2, imageAlt: "infographic 2" },
-  ]
+
 
   return (
     <>
       {/* Opret sidens indhold herunder som "normal HTML" eller referer til components*/}
       <h1>Forside</h1>
-      <Værdier blocks={værdierBlocks} />
+
     </>
   )
 }
