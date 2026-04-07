af3ca2107299534d97180c736a04b92a57a44773
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-07 22:30:12 +0200

    HTML til kontaktformular tilføjet

--- resurge-react-project/src/views/KontaktView.jsx
+++ resurge-react-project/src/views/KontaktView.jsx
@@ -8,7 +8,23 @@ function KontaktView() {
     <>
       {/* Opret sidens indhold herunder som "normal HTML" eller referer til components*/}
       <h1>Kontakt</h1>
-      <ComponentTemplate/>
+      <div>
+        </div>
+        <form className="flex flex-col">
+          <label htmlFor="navn">Navn:</label>
+          <input type="text" id="navn" name="navn" required />
+
+          <label htmlFor="telefon">Telefon:</label>
+          <input type="text" id="telefon" name="telefon" required />
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+
+          <label htmlFor="besked">Besked:</label>
+          <textarea id="besked" name="besked" rows="4" required></textarea>
+
+          <button type="submit">Send</button>
+        </form>
     </>
   )
 }
