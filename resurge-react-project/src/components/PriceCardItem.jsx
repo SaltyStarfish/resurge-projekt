6b5accd4fe3e29bdcd19aacb3a7e57c03e1b7fad
SaltyStarfish <eaa25stje@students.eaaa.dk>
2026-04-08 10:08:39 +0200

    Ydelser og priser opdateret med indhold, bedre spacing og bedre responsivitet

--- resurge-react-project/src/components/PriceCardItem.jsx
+++ resurge-react-project/src/components/PriceCardItem.jsx
@@ -3,7 +3,7 @@
 export default function PriceCardItem(card) {
 
   return (
-    <div className="flex flex-col flex flex-shrink-0 w-70 md:w-125 max-w-full rounded-xl relative overflow-hidden p-5 md:p-10">
+    <div className="flex flex-col flex flex-shrink-0 w-70 md:w-100 max-w-full rounded-xl relative overflow-hidden p-5 md:p-10">
       <img className="absolute inset-0 object-cover -z-1 w-full h-full" src={card.image} alt={card.title} />
       <h2 className="text-xl leading-tight md:text-[40px]">{card.title}</h2>
       <p className="text-xs leading-normal md:text-xl">{card.indhold}</p>
--- resurge-react-project/src/components/PriceCardSection.jsx
+++ resurge-react-project/src/components/PriceCardSection.jsx
@@ -9,26 +9,26 @@ const cards = [
   {
     image: logo, 
     title: "Performance-optimering",
-    indhold: "Få optimeret din eksisterende hjemmeside. Vi arbejder med billed-optimering, video-optimering, kode-optimering for at gøre din hjemmeside hurtigere.",
+    indhold: "Få optimeret din eksisterende hjemmeside. Vi arbejder med billed-optimering, video-optimering, kode-optimering m.m. for at gøre din hjemmeside hurtigere og forbedre brugeroplevelsen, så du rangerer bedre på Google og får flere kunder.",
     pris: "Pris fra 999,-",
   },
   {
     image: logo, 
     title: "Ny hjemmeside",
-    indhold: "Personlig tilpasset hjemmeside til din virksomhed og dine kunder. Kom med dine egne ideer, eller lad os hjælpe med alt fra indhold og branding til programmering og performance.",
+    indhold: "Personlig tilpasset hjemmeside til din virksomhed og din budget. Få en platform til at vise din virksomhed frem, gøre kontakten nem, oplyse om åbningstider, etc. Kom med dine egne ideer, eller lad os hjælpe med alt fra indhold, branding og opsætning til programmering og performance.",
     pris: "Pris fra 99,-/md. + 2999,-",
   },
   {
     image: logo, 
     title: "Ny webshop",
-    indhold: "Skræddersyet webshop til din virksomhed og dine kunder.",
+    indhold: "Skræddersyet webshop til din virksomhed og dine kunder. Vi hjælper dig med alt fra tilkobling af lagerstyring og betalingsløsninger til branding, programmering, performance og optimeret salgsflow. Lad os hjælpe dig med at skabe en brugervenlig og effektiv online butik.",
     pris: "Pris fra 199,-/md. + 3999,-",
   },
 ];
 
   return (
     <>
-      <div className='flex gap-3 md:gap-7.5 overflow-x-auto hide-scrollbar'>
+      <div className='flex gap-3 md:gap-7.5 overflow-x-auto hide-scrollbar px-6 md:px-12 lg:px-20'>
         {cards.map((card, index) => (
           <PriceCardItem key={index} {...card}/>
         ))}
