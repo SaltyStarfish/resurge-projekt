d73f9d3cd5fdae412ea7c3fd6e183cef5ab0525b
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-08 22:09:08 +0200

    Implementering af useState til at gemme og opdatere input + formData logges til konsollen, når formularen indsendes

--- resurge-react-project/src/views/KontaktView.jsx
+++ resurge-react-project/src/views/KontaktView.jsx
@@ -1,32 +1,95 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import CtaKnap from "../components/CtaKnap"
 
 function KontaktView() {
 
+  const [formData, setFormData] = useState({
+    navn: "",
+    telefon: "",
+    email: "",
+    besked: "",
+  });
+
+  {/* State til popup */ }
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    { /* Dataen logges i Console */ }
+    console.log("Data indsendt:", formData);
+
+    { /* Funktion til at åbne popup */ }
+    setPopupVisible(true);
+  }
+
   return (
     <>
-      {/* Opret sidens indhold herunder som "normal HTML" eller referer til components*/}
       <h1 className="font-h1 font-medium mb-4">Kontakt os</h1>
+
       <div className="flex justify-center w-full max-w-lg mx-auto">
-        <form className="flex flex-col bg-[#CEBC98] shadow-lg rounded-lg p-8 w-full">
+        <form onSubmit={handleSubmit}
+         className="flex flex-col bg-[#CEBC98] shadow-lg rounded-lg p-8 w-full">
+
           <h2 className="font-h2 font-medium mb-4">Kontaktformular</h2>
+
           <label htmlFor="navn" className="text-[#2B2B2B]">Navn:</label>
-          <input type="text" id="navn" name="navn" required placeholder="Skriv dit navn her" className="border-2 border-[#722E3C] rounded-md mb-4"
+          <input 
+          type="text"
+          name="navn"
+          required
+          value={formData.navn}
+          onChange={handleChange}
+          placeholder="Skriv dit navn her" 
+          className="border-2 border-[#722E3C] rounded-md mb-4"
        />
 
           <label htmlFor="telefon" className="text-[#2B2B2B]">Telefon:</label>
-          <input type="text" id="telefon" name="telefon" required placeholder="Hvilket nummer må vi kontakte dig på?" className="border-2 border-[#722E3C] rounded-md mb-4" />
+          <input 
+          type="text"
+          name="telefon" 
+          required
+          value={formData.telefon}
+          onChange={handleChange}
+          placeholder="Hvilket nummer må vi kontakte dig på?" 
+          className="border-2 border-[#722E3C] rounded-md mb-4" 
+          />
 
           <label htmlFor="email" className="text-[#2B2B2B]">E-mail:</label>
-          <input type="email" id="email" name="email" required placeholder="Skriv din e-mailadresse her" className="border-2 border-[#722E3C] rounded-md mb-4" />
+          <input 
+          type="email"
+          name="email" 
+          required
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Skriv din e-mailadresse her" 
+          className="border-2 border-[#722E3C] rounded-md mb-4" />
 
           <label htmlFor="besked" className="text-[#2B2B2B]">Besked:</label>
-          <textarea id="besked" name="besked" rows="4" required placeholder="Fortæl os gerne, hvilken ydelse du har brug for. Vi glæder os til at høre fra dig." className="border-2 border-[#722E3C] rounded-md mb-4" ></textarea>
+          <textarea 
+          name="besked" 
+          required 
+          value={formData.besked}
+          onChange={handleChange}
+          placeholder="Fortæl os gerne, hvilken ydelse du har brug for. Vi glæder os til at høre fra dig" className="border-2 border-[#722E3C] rounded-md mb-4"
+          rows="5"
+          />
+          
           <div className="max-w-105 mx-auto">
           <CtaKnap>
             <button type="submit" className="py-1 px-6 flex justify-center">Send besked</button>
           </CtaKnap>
           </div>
+          
         </form>
       </div>
     </>
