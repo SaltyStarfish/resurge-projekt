adf09eb11569c06983337df2fb36d75b3ac5ee4c
SaltyStarfish <eaa25stje@students.eaaa.dk>
2026-04-09 09:06:44 +0200

    lidt større h2 og h3 på mobil

--- resurge-react-project/src/components/CtaLink.jsx
+++ resurge-react-project/src/components/CtaLink.jsx
@@ -4,7 +4,7 @@ export default function CtaLink({ to, children }) {
   return (
     <RouterLink
       to={to}
-      className="inline-block mt-4 urbanist font-medium text-[26px] text-[#722E3C] transition-colors duration-300 hover:text-[#FFFFFF]"
+      className="inline-block mt-4 urbanist font-medium text-lg md:text-[26px] text-[#722E3C] transition-colors duration-300 hover:text-[#FFFFFF]"
     >
       {children} <span className="ml-1">→</span>
     </RouterLink>
