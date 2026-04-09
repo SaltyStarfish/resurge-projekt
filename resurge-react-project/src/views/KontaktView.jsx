070ce1f78f7a75cb70483f22d899f104dab1d516
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-09 10:51:04 +0200

    margin justeret

--- resurge-react-project/src/views/KontaktView.jsx
+++ resurge-react-project/src/views/KontaktView.jsx
@@ -36,8 +36,8 @@ function KontaktView() {
     <>
     <Container>
       <div className="flex flex-col w-full max-w-[1102px] mx-auto px-4 sm:px-6 md:px-0 my-[105px]">
-      <h1 className="font-h1 font-medium my-[105px]">Kontakt os</h1>
-          <p className="urbanist text-[30px]">
+      <h1 className="font-h1 font-medium">Kontakt os</h1>
+          <p className="urbanist text-[30px] mt-[40px]">
             Har du et projekt, en idé eller blot et spørgsmål? Hiv fat i os, og få et uforpligtende tilbud.
           </p>
 
