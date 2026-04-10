6f21b40d33481df99f24bbb24f7f07298f7c5cb7
SaltyStarfish <eaa25stje@students.eaaa.dk>
2026-04-10 11:59:18 +0200

    links aligned i scrollable case-cards

--- resurge-react-project/src/components/SmallerCaseCard.jsx
+++ resurge-react-project/src/components/SmallerCaseCard.jsx
@@ -1,22 +1,19 @@
 import CtaLink from "./CtaLink";
 
-export default function SmallerCaseCard({ image, tags = [], title, description, link }) {
+export default function SmallerCaseCard({ image, tags = [], title, link }) {
   return (
-    <div className="flex flex-col">
-      <img src={image} alt={title} className="w-full w-[495] h-[374px] rounded-[20px] object-cover object-center mx-auto" />
+    <div className="flex-grow-1 flex flex-col">
+      <img src={image} alt={title} className="w-70 md:w-100 max-w-full h-[374px] rounded-[20px] object-cover object-center mx-auto" />
       
       {tags.length > 0 && (
         <p className="font-tag font-medium mt-5 md:mt-10">
           {tags.join(" | ")}
         </p>
         )}
-
-      <div className="flex flex-col items-start text-left">
         
-        <div className="mt-5 md:mt-10">
+        <div className="mt-auto">
           <CtaLink to={link}><h3 className="inline font-h3 font-medium">{title}</h3></CtaLink>
         </div>
-      </div>
     </div>
   );
 } 
