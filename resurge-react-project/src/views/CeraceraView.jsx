bd131cfef6ce06420db1a26673f7edb80b2f18c7
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-04-11 15:33:55 +0200

    Sektion slettet for at gøre det mindre teksttungt

--- resurge-react-project/src/views/CeraceraView.jsx
+++ resurge-react-project/src/views/CeraceraView.jsx
@@ -40,15 +40,11 @@ function CeraceraView() {
   return (
    <Container>
       <div className="flex flex-col w-full max-w-[1102px] mx-auto px-4    sm:px-6 md:px-0 my-[105px]">
-          <h1 className="font-h1 font-medium">CeraCera</h1>
-            <p className="urbanist font-intro mt-[40px]">
-              Ny webshop til keramikværksted med fokus på enkel booking, nem vedligeholdelse af siden og en effektiv brugeroplevelse.
+          <h1 className="font-h1 font-medium">CeraCera: Ny webshop og bookingløsning</h1>
+            <p className="urbanist font-intro mt-[40px] mb-[105px]">
+              CeraCera er et keramikværksted i Aarhus, der sælger egne kreationer og afholder keramikkurser. De ønskede en løsning, der i højere grad understøtter deres fysiske butik og gør det nemt for dem at håndtere både onlinesalg og bookinger.
             </p>
-
-          <section className="mt-[105px]">
-            <h2 className="font-h2 mb-3.75 md:mb-7.5">Webshop- og bookingløsning til aarhusiansk keramikværksted</h2>
-            <p className="font-body mb-4.5 md:mb-9">CeraCera er et keramikværksted i Aarhus, der sælger egne kreationer og afholder keramikkurser. De ønskede en løsning, der i højere grad understøtter deres fysiske butik og gør det nemt for dem at håndtere både onlinesalg og bookinger.</p>
-          </section>
+            
           <Infographic blocks={CeraCera} />
         <div className="flex flex-col gap-[80px] mt-[105px]">
           {indhold.map((block, i) => (
